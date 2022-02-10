68be8a1a9951aa7d26ec03e9539257643f00feea
Nenad Misic <n.misic@outlook.com>
2022-02-10 21:15:19 +0100

    Add profile name

--- src/components/Dashboard/FAB.js
+++ src/components/Dashboard/FAB.js
@@ -1,16 +1,21 @@
 import { FitnessCenter, Menu as MenuIcon, NoteAdd } from "@mui/icons-material";
 import Logout from "@mui/icons-material/Logout";
 import { Button } from "@mui/material";
+import Avatar from "@mui/material/Avatar";
 import Box from "@mui/material/Box";
 import Divider from "@mui/material/Divider";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import Menu from "@mui/material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import Tooltip from "@mui/material/Tooltip";
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { UserContext } from "../../providers";
 
 export default function FAB(props) {
   const { onAddNote, onAddBlock, onSignOut } = props;
+  const { user } = useContext(UserContext);
+
+  console.log(user);
 
   const [anchorEl, setAnchorEl] = useState(null);
   const handleClick = (event) => setAnchorEl(event.currentTarget);
@@ -56,28 +61,42 @@ export default function FAB(props) {
             overflow: "visible",
             filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
             mt: 1.5,
-            width: 180,
+            width: 220,
           },
         }}
         transformOrigin={{ horizontal: "right", vertical: "bottom" }}
         anchorOrigin={{ horizontal: "right", vertical: "top" }}
       >
+        <MenuItem>
+          <Avatar />
+          <p
+            style={{
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap",
+              overflow: "hidden",
+              width: 210,
+            }}
+          >
+            {user.displayName}
+          </p>
+        </MenuItem>
+        <Divider />
         <MenuItem sx={{ fontSize: 20 }} onClick={onAddBlock}>
           <ListItemIcon>
-            <FitnessCenter fontSize="medium" />
+            <FitnessCenter />
           </ListItemIcon>
           Add block
         </MenuItem>
         <MenuItem onClick={onAddNote}>
           <ListItemIcon>
-            <NoteAdd fontSize="medium" />
+            <NoteAdd />
           </ListItemIcon>
           Add note
         </MenuItem>
         <Divider />
         <MenuItem onClick={onSignOut}>
           <ListItemIcon>
-            <Logout fontSize="medium" />
+            <Logout />
           </ListItemIcon>
           Sign out
         </MenuItem>
