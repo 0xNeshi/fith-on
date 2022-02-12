c799adc002a276084129ef317fa9e88573ffd8d0
Nenad Misic <n.misic@outlook.com>
2022-02-12 16:08:57 +0100

    Remove logs from guards.js

--- src/guards.js
+++ src/guards.js
@@ -4,12 +4,10 @@ import { UserContext } from "./providers";
 
 export function RequireAuth({ children }) {
   const { user } = useContext(UserContext);
-  console.log("auth", user);
   return !!user ? children : <Navigate to="/signin" />;
 }
 
 export function RequireAnon({ children }) {
   const { user } = useContext(UserContext);
-  console.log("anon", user);
   return !user ? children : <Navigate to="/dashboard" />;
 }
