13c19c4b8cc823eff3b068da926262206dd41d78
Nenad Misic <n.misic@outlook.com>
2022-02-13 18:51:43 +0100

    Remove RestoreData

--- src/components/Modals/index.js
+++ src/components/Modals/index.js
@@ -2,4 +2,3 @@ export { default as AddBlock } from "./AddBlock";
 export { default as AddNote } from "./AddNote";
 export { default as DeleteSection } from "./DeleteSection";
 export { default as SignOut } from "./SignOut";
-export { default as RestoreData } from "./RestoreData";
