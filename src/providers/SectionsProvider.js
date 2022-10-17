59236f0b6d2b0c0985f12546ba62ef073f1373f1
Nenad Misic <n.misic@outlook.com>
2022-10-17 08:37:33 +0200

    Fix missing NetworkStateContext import in SectionsProvider

--- src/providers/SectionsProvider.js
+++ src/providers/SectionsProvider.js
@@ -6,7 +6,7 @@ import {
   useState,
 } from "react";
 import { v4 as uuidv4 } from "uuid";
-import { UserContext } from "../providers";
+import { NetworkStateContext, UserContext } from "../providers";
 import {
   addSection,
   getSections,
