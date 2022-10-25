f2861e58e020637f9316070dac8df50fdb94aeab
Nenad Misic <n.misic@outlook.com>
2022-10-25 11:36:26 +0200

    Add userEmail check to logf

--- src/services/log.js
+++ src/services/log.js
@@ -2,6 +2,15 @@ import { addDoc, collection } from "firebase/firestore";
 import { db } from "../config/firebase";
 
 const logf = async (userEmail, action, message) => {
+  if (!userEmail) {
+    console.log("logf: userEmail is undefined/null at ", action);
+    await addDoc(collection(db, "logs"), {
+      action: "logf",
+      message: `userEmail is undefined/null at ${action}`,
+      timestamp: new Date().toLocaleDateString("en-GB"),
+    });
+  }
+
   await addDoc(collection(db, "logs"), {
     userEmail,
     action,
