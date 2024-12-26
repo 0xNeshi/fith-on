c3984abc0b40454ec95e4f8bf616c03018452a10
0xNeshi <nenad.misic@openzeppelin.com>
2024-12-26 08:59:19 +0100

    add deadlift and leg press

--- src/constants.js
+++ src/constants.js
@@ -5,6 +5,12 @@ export const EXERCISE_KEYS = {
   BarbellRow: "barbell row",
   Bench: "bench",
   Deadlift: "deadlift",
+  LegPress: "leg press",
+  /**
+   * Actually: Bulgarian Split Squat.
+   * Used the shorter (inaccurate) name for better formatting.
+   */
+  SplitSquat: "split squat",
 };
 
 export const EXERCISES = Object.values(EXERCISE_KEYS);
